ee3fff6dc9e5dc25b008ff97726accdff23a06ee
Anurag Gupta <anuraggupta5086@gmail.com>
2022-07-22 23:04:00 +0530

    done

--- 21july_todolist/index.js
+++ 21july_todolist/index.js
@@ -4,17 +4,34 @@ const todoul = document.getElementById("todos");
 
 const todos = JSON.parse(localStorage.getItem("todoString"));
 
-if (todos) {
-  todos.forEach((todo) => {
-    addTodo(todo);
-  });
-}
+const updateLS = () => {
+  const allTodos = document.querySelectorAll("li");
 
-const addTodo = () => {
+  const todosArr = [];
+
+  allTodos.forEach((todoEl) => {
+    todosArr.push({
+      text: todoEl.innerText,
+      isCompleted: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todoString", JSON.stringify(todosArr));
+};
+
+const addTodo = (todo) => {
   let todotext = input.value;
+
+  if (todo) {
+    todotext = todo.text;
+  }
 
   if (todotext) {
     const todoElement = document.createElement("li");
+
+    if (todo && todo.isCompleted) {
+      todoElement.classList.add("completed");
+    }
 
     todoElement.innerText = todotext;
     todoElement.addEventListener("click", () => {
@@ -22,12 +39,24 @@ const addTodo = () => {
       updateLS();
     });
 
-    // todoElement.addEventListener("contextmenu", () => {
-    //   todoElement.remove();
+    todoElement.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoElement.remove();
+      updateLS();
+    });
 
-    // });
+    todoul.appendChild(todoElement);
+    input.value = "";
+
+    updateLS();
   }
 };
+
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo);
+  });
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
